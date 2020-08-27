80fa35360aa17c760285d1ebd92ee2b67ed03c93
boazka laptopja <kontab6@gmail.com>
2020-08-27 14:09:08 +0200

    alakulo html probalkozas

--- ma2html_parser/xtracthtml.py
+++ ma2html_parser/xtracthtml.py
@@ -3,12 +3,14 @@ from bs4 import BeautifulSoup
 
 
 file = open('C:\\ProgramData\\MA Lighting Technologies\\grandma\\gma2_V_3.9.51\\lang\\hu\\help\\structure.html', 'r')
-html_doc = file
-soup = BeautifulSoup(html_doc, 'html.parser')
+soup = BeautifulSoup(file, 'html.parser')
+for x in soup.find_all('li'):
+    print(x.attrs['id'])
+    print(x.attrs['rel'])
+    print(x.attrs['data-id'])
+    print(x.attrs['title'])
+    print(x.contents[0])
+    print(x.attrs['data-keyword'])
 
-for x in soup.ul.li:
-    try:
-        print(x.attrs)
-    except AttributeError:
-        x.next_sibling()
+
 
