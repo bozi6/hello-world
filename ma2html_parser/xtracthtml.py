45a85122b2edd5f7d3da68340f7f0afd06ba90d8
boazka laptopja <kontab6@gmail.com>
2021-01-15 10:23:48 +0100

    new file:   filshashing2sql/adatbazis/__pycache__/adatbazis.cpython-38.pyc 	new file:   filshashing2sql/adatbazis/__pycache__/insert_multi_row.cpython-38.pyc 	new file:   filshashing2sql/adatbazis/__pycache__/python_mysql_dbconfig.cpython-38.pyc 	new file:   ma2html_parser/mahtm2sql.sql 	modified:   ma2html_parser/xtracthtml.py 	new file:   regturka/reg.py

--- ma2html_parser/xtracthtml.py
+++ ma2html_parser/xtracthtml.py
@@ -1,16 +1,16 @@
-
 from bs4 import BeautifulSoup
 
-
+sql = "INSERT INTO help (sorsz, id, rel, `data-id`, title, class, `data-keyword`, `data-jstree`) VALUES "
+sqlfile = open('./mahtm2sql.sql', 'w+')
 file = open('C:\\ProgramData\\MA Lighting Technologies\\grandma\\gma2_V_3.9.51\\lang\\hu\\help\\structure.html', 'r')
 soup = BeautifulSoup(file, 'html.parser')
 for x in soup.find_all('li'):
-    print(x.attrs['id'])
-    print(x.attrs['rel'])
-    print(x.attrs['data-id'])
-    print(x.attrs['title'])
-    print(x.contents[0])
-    print(x.attrs['data-keyword'])
-
-
+    sql += (
+        f"(NULL, '{x.attrs['id']}', '{x.attrs['rel']}', '{x.attrs['data-id']}', '{x.attrs['title']}', '{x.attrs['class']}','{x.attrs['data-keyword']}', '{x.attrs['data-jstree']}'), ")
+sql = sql[:-2]
+sql = sql+';'
+sqlfile.write(sql)
+sqlfile.close()
+file.close()
+print('ok')
 
