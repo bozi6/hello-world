975cc4dfba00e8c9f54e9ec4c108763db4261831
kontab6@gmail.com <Github?S0nn3licht>
2021-10-30 18:44:00 +0200

    dmx2100 calc inserted

---
+++ dmx2percent/main.py
@@ -0,0 +1,15 @@
+def szamol(dmx):
+    try:
+        if dmx > 255:
+            print('Nem lehet nagyobb 255 nél.')
+            return 0
+        ered = (dmx / 255) * 100
+        return ered
+    except ValueError:
+        return 0
+
+
+while True:
+    cuc = input(' dmx:')
+    a = szamol(int(cuc))
+    print('{:.2f}'.format(a))
