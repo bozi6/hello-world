a4feb731dc3c15f529a716f073ac6a23532cb62c
kontab6@gmail.com <Github?S0nn3licht>
2022-04-13 20:55:31 +0200

    szorzos graph

---
+++ x3meg1/main.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import random
+# szam = int(input("szám"))
+vonalszin = ['blue', 'red', 'yellow', 'green', 'black', 'gray', 'autumn', 'bone', 'cool', 'copper', 'flag',
+             'hot', 'hsv', 'jet', 'pink', 'prism', 'spring', 'summer', 'winter']
+plt.title('Ha x páratlan akkor * 3+1 ha páros akkor / 2')
+rajz = []
+for i in range(100):
+    szam = random.randint(1, 20)
+    rajz.append(szam)
+    print(i, szam)
+    while szam > 1:
+        if (szam % 2) == 1:
+            szam = szam * 3 + 1
+        else:
+            szam = szam // 2
+        rajz.append(szam)
+    plt.plot(rajz, label='számok', color=random.shuffle(vonalszin))
+    rajz.clear()
+plt.xlabel('Lépések')
+plt.ylabel('Számok')
+plt.show()
