53174749b81014ac97a9c122e3b10de11136cf05
kontab6@gmail.com <Github?S0nn3licht>
2022-08-29 23:58:44 +0200

    translated

--- x3meg1/main.py
+++ x3meg1/main.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as plt
 import random
-# szam = int(input("szám"))
-import pylab as pl
+
 
 vonalszin = ['blue', 'red', 'yellow', 'green', 'black', 'gray', 'autumn', 'bone', 'cool', 'copper', 'flag',
              'hot', 'hsv', 'jet', 'pink', 'prism', 'spring', 'summer', 'winter']
-plt.title('Ha x páratlan akkor * 3+1 ha páros akkor / 2')
+plt.title('If x odd then (num * 3+1), if even then (num/ 2)')
 rajz = []
 for i in range(10):
     szam = random.randint(1, 20)
@@ -17,8 +16,8 @@ for i in range(10):
         else:
             szam = szam // 2
         rajz.append(szam)
-    plt.plot(rajz, label='számok', color=random.shuffle(vonalszin))
+    plt.plot(rajz, label='numbers', color=random.shuffle(vonalszin))
     rajz.clear()
-plt.xlabel('Lépések')
-plt.ylabel('Számok')
+plt.xlabel('Steps')
+plt.ylabel('Numbers')
 plt.show()
