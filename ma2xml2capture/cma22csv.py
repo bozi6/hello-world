da5d0f1400b07fed639ea86deda9082ef5617a31
boazka laptopja <kontab6@gmail.com>
2023-02-15 09:30:44 +0100

    cma22csv minor changes

--- ma2xml2capture/cma22csv.py
+++ ma2xml2capture/cma22csv.py
@@ -3,11 +3,11 @@ import xml.etree.ElementTree as Et
 import csv
 import lampak
 
-bedir = './xmlz/'
-kidir = './csvk/'
-bef = 'ntsz_old.xml'
-befile = bedir + bef
-kifile = kidir + bef + "_conv.csv"
+input_dir = './xmlz/'
+output_dir = './csvk/'
+input_file = 'ntsz_old.xml'
+input_path = input_dir + input_file
+output_path = output_dir + input_file + "_conv.csv"
 # Ezek a mezők vannak a capture csv-ben
 fields = ['Fixture', 'Optics', 'Wattage', 'Unit', 'Circuit', 'Channel',
           'Groups', 'Patch', 'DMX Mode', 'DMX Channles', 'Layer', 'Focus',
@@ -18,10 +18,10 @@ fields = ['Fixture', 'Optics', 'Wattage', 'Unit', 'Circuit', 'Channel',
           'Tilt End Limit', 'Identifier', 'External Identifier']
 
 data = []
-mytree = Et.parse(befile)
+mytree = Et.parse(input_path)
 
 # namespace definiálása az ma2 exportált xml alapján
-ns = {'bas': "http://schemas.malighting.de/grandma2/xml/MA",
+namespace = {'bas': "http://schemas.malighting.de/grandma2/xml/MA",
       'xsi': "http://www.w3.org/2001/XMLSchema-instance",
       'schema': "http://schemas.malighting.de/grandma2/xml/MA http://schemas.malighting.de/grandma2/xml/3.9.60/MA.xsd"}
 
@@ -31,46 +31,46 @@ print('Showfile neve: {}'.format(myroot[0].attrib['showfile']))
 print("MA2 programverzió: {},{},{}".format(
     myroot.attrib['major_vers'], myroot.attrib['minor_vers'], myroot.attrib['stream_vers']))
 
-for Layer in myroot.findall("bas:Layer", ns):
-    rn = Layer.attrib['name']  # Réteg nevének és indexének kinyerése
+for Layer in myroot.findall("bas:Layer", namespace):
+    layer_name = Layer.attrib['name']  # Réteg nevének és indexének kinyerése
     ridx = Layer.attrib['index']
     uid = uuid.uuid4()
     for Fixture in Layer:  # Lámpákon szaladunk végig.
-        egylampa = lampak.Lampa(Fixture[0].attrib['name'])
-        egylampa.Unit = Fixture.attrib['name']
-        egylampa.Layer = rn
-        egylampa.extidentifier = uid
-        fi = Fixture.attrib['index']  # Lámpa index és név
+        onefixture = lampak.Lampa(Fixture[0].attrib['name'])
+        onefixture.Unit = Fixture.attrib['name']
+        onefixture.Layer = layer_name
+        onefixture.extidentifier = uid
+        fixture_index = Fixture.attrib['index']  # Lámpa index és név
         ''''#  Itt csak átalakítjuk a mostani fileban a macet gagyibbra lustaságból!
         if egylampa.Fixture[:8] == 'Mac700PB':  # levágjuk a sorszámot a lámpanévből
             egylampa.Fixture = 'Martin MAC 250 Entour'  # jól kicseréljük a capture student verzióval
         '''
-        egylampa.Patch = Fixture[1][0][0].text  # Fixture/subfixture/patch szövege
-        if egylampa.Patch != '0':  # ha nincs dmx címe a cuccnak akkor az előzőt adjuk a mdmxnek
-            mdmx = egylampa.Patch
+        onefixture.Patch = Fixture[1][0][0].text  # Fixture/subfixture/patch szövege
+        if onefixture.Patch != '0':  # ha nincs dmx címe a cuccnak akkor az előzőt adjuk a mdmxnek
+            mdmx = onefixture.Patch
         fpos = Fixture[1][1][0].attrib  # Fixture/subfixture/absoluteposition/attribjai
-        egylampa.posx = fpos['x'] + 'm'
-        egylampa.posy = fpos['y'] + 'm'
-        egylampa.posz = fpos['z'] + 'm'
+        onefixture.posx = fpos['x'] + 'm'
+        onefixture.posy = fpos['y'] + 'm'
+        onefixture.posz = fpos['z'] + 'm'
         frot = Fixture[1][1][1].attrib
-        egylampa.rotx = frot['x']+'°'
-        egylampa.roty = frot['y']+'°'
-        egylampa.rotz = frot['z']+'°'
+        onefixture.rotx = frot['x'] + '°'
+        onefixture.roty = frot['y'] + '°'
+        onefixture.rotz = frot['z'] + '°'
         if 'fixture_id' in Fixture.attrib:  # Ha robotlámpa akkor ma2 szerint fixture
-            egylampa.Channel = Fixture.attrib['fixture_id']
+            onefixture.Channel = Fixture.attrib['fixture_id']
         if 'channel_id' in Fixture.attrib:  # ha dimmer akkor channel ma2 szerint
-            egylampa.Channel = Fixture.attrib['channel_id']
+            onefixture.Channel = Fixture.attrib['channel_id']
         if 'is_multipatch' in Fixture.attrib:  # ha multipatchelt a lámpa
-            egylampa.Patch = mdmx
+            onefixture.Patch = mdmx
 
-        data.append(egylampa.lamplista())
+        data.append(onefixture.lamplista())
 
 
-with open(kifile, 'w', newline='') as csvfile:
+with open(output_path, 'w', newline='') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     filewriter.writerow(fields)
     filewriter.writerows(data)
 
 csvfile.close()
-print("Az importálható cucc a {} -ban található.".format(kifile))
+print("Az importálható cucc a {} -ban található.".format(output_path))
 print("Ja amúgy kész vagyok...")
