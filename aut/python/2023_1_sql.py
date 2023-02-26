eb3bea6aeb81e07a39834f6bed1f69d4529bb737
Konta Boáz <kontab6@gmail.com>
2023-02-26 20:51:17 +0100

    panda autentikusban elkezdve

--- aut/python/2023_1_sql.py
+++ aut/python/2023_1_sql.py
@@ -1,8 +1,12 @@
+import numpy as np
 import pandas as pd
 
 bem = "../xlsxs/2023_Autentikus.xlsx"
 kim = "../sql/2023_aut.sql"
 sql = ''
+honapok = ['JANUÁR', 'FEBRUÁR', 'MÁRCIUS', 'ÁPRILIS', 'MÁJUS',
+           'JÚNIUS', 'JÚLIUS', 'AUGUSZTUS', 'SZEPTEMBER', 'OKTÓBER',
+           'NOVEMBER', 'DECEMBER']
 
 
 def sor(uzi):
@@ -24,10 +28,21 @@ f.write('USE honved2;\n')
     Itt történne az autentikus felodgozása
 '''
 
-Autentikus = pd.read_excel(bem)
+Autentikus = pd.read_excel(bem, usecols="A,C:J")
 sor("head rész")
-print(Autentikus.head(6))
-sor("index")
-print(Autentikus.index)
-print(Autentikus.columns[Autentikus.isnull().any()])
-print(Autentikus["Unnamed: 0"].to_list())
+# print(Autentikus.to_string())
+sor("Előtte")
+print(Autentikus.info())
+Tanckar = Autentikus.dropna(subset=['Tánckar', 'Unnamed: 0']) # eldobtuk azokat a sorokat ahol nincs a tánckar kitöltve.
+sor("Első dobás")
+print(Tanckar.info())
+print(Tanckar.to_string())
+print(Tanckar.shape)
+print(Tanckar.columns)
+
+''' Linkek:
+https://www.w3schools.com/python/pandas/pandas_cleaning_wrong_data.asp
+https://pythonguides.com/pandas-drop/
+https://pandas.pydata.org/docs/user_guide/10min.html
+https://www.w3schools.com/python/python_tuples.asp
+'''
