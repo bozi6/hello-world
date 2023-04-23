e67144abb1551dd62cc7ad87b8ebb2db47566edb
Konta Boáz <kontab6@gmail.com>
2023-04-23 23:13:09 +0200

    aut 23 frissités

--- aut/python/2023_1_sql.py
+++ aut/python/2023_1_sql.py
@@ -1,81 +1,135 @@
-import numpy as np
-import pandas as pd
+#!/usr/bin/env python3
+import re
+import datetime
+import openpyxl
+import sqlescapy
+import logging
+import time
+import funct.funct as adatok
 
-bem = "../xlsxs/2023_Autentikus.xlsx"
-kim = "../sql/2023_aut.html"
-sql = ''
-honapok = ('JANUÁR', 'FEBRUÁR', 'MÁRCIUS', 'ÁPRILIS', 'MÁJUS',
-           'JÚNIUS', 'JÚLIUS', 'AUGUSZTUS', 'SZEPTEMBER', 'OKTÓBER',
-           'NOVEMBER', 'DECEMBER')
-
-
-def sor(uzi):
-    emptyspaces = 80
-    print("-" * emptyspaces)
-    print(uzi.center(emptyspaces))
-    print("-" * emptyspaces)
+KezdesiIdo = time.time()
 
 
-'''
-    Itt megnyitjuk a kimeneti sql fájlt a megfelelő qsl 
-    parancs létrehozásához
-'''
-f = open(kim, "w", encoding='utf8')
-# f.write('# Honvédelmi adatok 2023-ra az autentikusból\n')
-# f.write('# Készítette: Konta Boáz (kontab6@gmail.com).\n')
-# f.write('USE honved2;\n')
-f.write('''<!doctype html>
-            <html lang="hu">
-            <head>
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-            </head>
-            <body class="container text-bg-secondary text-black">
-            ''')
-'''
-    Itt történne az autentikus felodgozása
-'''
+logging.basicConfig(level=logging.INFO, format=' %(asctime)s  - %(message)s')
+# logging.disable(logging.DEBUG)  # Akkor kell ha már nem akarunk Debuggolni. :-)
+# logging.disable(logging.INFO)
+# logging.info('Program elkezdődött.')
 
-Autentikus = pd.read_excel(bem, usecols="A,C:J")
-Autentikus.rename(columns={'Unnamed: 0': 'datum',
-                           'Tánckar': 'tanckar',
-                           'Zenekar önálló': 'zkr',
-                           'FÉRFIKAR': 'ffkar',
-                           'KÖZREMŰK. EGY. ALATT/ KÜLSŐS': 'kozrem',
-                           'KONTAKT': 'kontakt',
-                           'STÁTUSZ': 'status',
-                           'KÜLSŐS SZÁLLÍTÁS': 'kulszall',
-                           'MEGJEGYZÉS': 'comment'}, inplace=True)  # Oszlopok átnevezése
-Autentikus.dropna(how='all', inplace=True)  # Kidobunk minden töküres sort
-print(Autentikus.columns)
-Autentikus = Autentikus[Autentikus.tanckar.str.contains('Tánckar') == False]
-print(Autentikus.to_string())
-#Autentikus.dropna(thresh=2, inplace=True)
+BemenetFile = "../xlsxs/2023_Autentikus.xlsx"
+KimenetFile = "../sql/2023_aut.sql"
+SqlSor = ''
 
-Autentikus.replace('\n', '<br>', inplace=True)
-AutDropped = Autentikus.to_html(classes="table-sm table-bordered", na_rep='-')
-#  htmlkesz = AutDropped.replace('\n', '<br>')
-htmlkesz = AutDropped
-f.write(htmlkesz)
-f.write('</body>\n</html>')
 
-exit(0)
-sor("Alap")
-print(Autentikus.info())
-print("Shape: ", Autentikus.shape)
-sor("Összesítő a hiányzó értékekről")
-print(Autentikus.ne(honapok))
-Tanckar = Autentikus.dropna(subset=['Unnamed: 0'])  # eldobtuk azokat a sorokat ahol nincs a tánckar.
-sor("Első dobás")
-print(Tanckar.info())
-print(Tanckar.to_string())
-print("Shape: ", Tanckar.shape)
-print(Tanckar.columns)
+kiiroFajl = open(KimenetFile, 'w', encoding='utf8')
+kiiroFajl.write('# Honvédelmi adatok 2023-ra az autentikusból\n')
+kiiroFajl.write('# Készítette: Konta Boáz (kontab6@gmail.com).\n')
+kiiroFajl.write('USE honved2;\n')
 
-''' Linkek:
-https://www.w3schools.com/python/pandas/pandas_cleaning_wrong_data.asp
-https://pythonguides.com/pandas-drop/
-https://pandas.pydata.org/docs/user_guide/10min.html
-https://www.w3schools.com/python/python_tuples.asp
-'''
+print('Bemeneti fájl: ' + BemenetFile)
+print('Kimenetei fájl: ' + KimenetFile)
+
+WorkBook = openpyxl.load_workbook(filename=BemenetFile, read_only=True)
+# read_only elvileg gyorsabb és amúgy sem akarunk írni bele.
+for sh in WorkBook.worksheets:  # Végigmegyünk a munkafüzet lapjain
+    cells = sh['A2':'J210']  # I210 a vége
+    i = 0
+    ''' Az értékek a következők:
+    c1  - Dátum ( óraperc nélkül )
+    c2  - Napok???
+    c3  - Tánckar és Zenekar
+    c4  - Zenekar önálló
+    c5  - Férfikar
+    c6  - Közreműködők egyeztetés alatt
+    c7  - Kontakt
+    c8  - Státusz
+    c9  - Külsős szállítás
+    c10  - megjegyzés
+    Ezek a 2022_AUTENTIKUS.xlsx táblázat fejlécsorának összetevői.
+    Továbbá! Közhírré tétetik!
+    Az excel fileban a dátum mezőt tessék rendesen beállítani.
+    '''
+    print("Munkalap neve: ", sh.title)
+    for c1, c2, c3, c4, c5, c6, c7, c8, c9, c10 in cells:
+        egyadat = adatok.Bemeno(c1, c2, c3 ,c4 ,c5, c6, c7, c8, c9, c10, None)
+        if (c1.value and c3.value) and (c3.value != 'Tánckar'):  # dátum tánckar kitöltve
+            result = re.match(r'^\d+.\d+.\d+', str(c1.value))
+            if result:
+                if isinstance(c1.value, datetime.date):
+                    egyadat.datum = c1.value
+                    d = c1.value.strftime('%Y-%m-%d')  # d = datum
+                elif c1.value not in egyadat.honapok:
+                    d = c1.value[0:6].replace(' ', '')
+                    d = d[0:5].replace('.', '-')
+                    logging.info('Ezek az érdekes dátumok: {}'.format(d))
+                    # d = c1.value[0:6].replace('.', '-')
+                    d = '2023-' + d.strip()
+                    # ha string => .->- és marad az első 10 karakter
+                    logging.debug('Datum mező:{} , típusa:{} '.format(d, type(d)))
+                    logging.info('''Változók értéke:
+                                            Dátum: {}
+                                            Napok: {}
+                                            Tánc: {}
+                                            Zkr: {}
+                                            FFikar: {}
+                                            Egyeztet: {}
+                                            Kontakt: {}
+                                            Státusz: {}
+                                            Külszáll: {}
+                                            Megjegy: {}'''
+                                 .format(c1.value, c2.value, c3.value, c4.value, c5.value,
+                                         c6.value, c7.value, c8.value, c9.value, c10.value))
+                SqlSor = "INSERT INTO aut (sorsz,datum,ceg,kezd,hely,musor,kontakt,megjegyzes,helykod) VALUES ( NULL,"
+                c2db = c3.value.split('/')  # A 0 az időpont/helyszín, az 1 pedig a műsor.
+                idopont = re.match("[0-9][0-9].?[0-9][0-9]", c2db[0])
+                try:
+                    c2db[1] = c2db[1].strip()
+                    musor = sqlescapy.sqlescape(c2db[1])
+                    musor = " ".join(musor.split())
+                    # musor = re.escape(c2db[1])
+                except IndexError:
+                    musor = 'Nincs megadva műsor.'
+                    logging.debug('IndexError - Nincs megadva műsor.')
+                if idopont:
+                    logging.debug('Van időpont!')
+                    # kezdes = c1.value.replace(hour=int(idopont.group()[:2]), minute=00)
+                    kezdes = idopont.group()
+                    hely = c2db[0].replace(kezdes, '', 1)
+                    hely = " ".join(hely.split())
+                    # hely elejéről levesszük a spacet
+
+                    logging.debug('Helyszín eredménye: ' + hely)
+                else:
+                    kezdes = 'Nincs megadva kezdés.'
+                    hely = c2db[0]
+                    logging.debug('Kezdés eredménye: ' + hely)
+                    # kezdes = c1.value.replace(hour=00, minute=00)
+                egyadat.kont = c7.value
+                kontakt = egyadat.kont
+                egyadat.megjegy = c10.value
+                megjegyzes = egyadat.megjegy
+                datum = d
+                logging.debug('Kezdési időpont kialakult: ' + str(kezdes))
+                SqlSor += '"'
+                SqlSor += str(datum)  # Dátum
+                SqlSor += '",'
+                SqlSor += '"24","'  # Cég
+                SqlSor += kezdes  # Kezdes
+                SqlSor += '","'
+                SqlSor += hely.strip()  # Hely
+                SqlSor += '","'
+                SqlSor += musor.strip()  # Musor
+                SqlSor += '","'
+                SqlSor += kontakt.strip()  # Kontakt
+                SqlSor += '","'
+                SqlSor += megjegyzes  # Megjegyzés
+                SqlSor += '","'
+                SqlSor += str(0)  # helykod
+                SqlSor += '");\n'
+                logging.debug(SqlSor)
+                kiiroFajl.write(SqlSor)
+                i = i + 1  # feldolgozott sorok száma.
+    print('{} sor feldolgozva.'.format(i))
+kiiroFajl.close()
+print('Fájl kiírása befejezve.')
+VegeIdo = time.time() - KezdesiIdo
+print('{:.5f}. sec alatt lefutott'.format(VegeIdo))
--- aut/python/funct/funct.py
+++ aut/python/funct/funct.py
@@ -1,60 +1,143 @@
-class Bemeno(object):
-    def __init__(self):
+import datetime
+import pprint
+from datetime import date
+
+class Bemeno:
+    def __init__(self, datum, napok: None, tkzkr: None, zkr: None, ffk: None,
+                 kuls: None, kont: None, stat: None, kulsz: None, megjegy: None, tev: None):
         self.honapok = ['JANUÁR', 'FEBRUÁR', 'MÁRCIUS',
                         'ÁPRILIS', 'MÁJUS', 'JÚNIUS',
                         'JÚLIUS', 'AUGUSZTUS', 'SZEPTEMBER',
                         'OKTÓBER', 'NOVEMBER', 'DECEMBER']
-        self._erdekes = {'datum': '',
-                         'napok': '',
-                         'tkzkr': '',
-                         'zkr': '',
-                         'ffk': '',
-                         'kuls': '',
-                         'kont': '',
-                         'stat': '',
-                         'kulsz': '',
-                         'megjegy': '',
+
+        self._erdekes = {'datum': datum,
+                         'napok': napok,
+                         'tkzkr': tkzkr,
+                         'zkr': zkr,
+                         'ffk': ffk,
+                         'kuls': kuls,
+                         'kont': kont,
+                         'stat': stat,
+                         'kulsz': kulsz,
+                         'megjegy': megjegy,
+                         'tev': tev,
                          }
 
     @property
     def datum(self):
         return self._erdekes['datum']
 
+    @property
     def napok(self):
         return self._erdekes['napok']
 
+    @property
     def tkzkr(self):
         return self._erdekes['tkzkr']
 
+    @property
     def zkr(self):
         return self._erdekes['zkr']
 
+    @property
     def ffk(self):
         return self._erdekes['ffk']
 
+    @property
     def kuls(self):
         return self._erdekes['kuls']
 
+    @property
     def kont(self):
         return self._erdekes['kont']
 
+    @property
     def stat(self):
         return self._erdekes['stat']
 
+    @property
     def kulsz(self):
         return self._erdekes['kulsz']
 
+    @property
     def megjegy(self):
         return self._erdekes['megjegy']
 
+    @property
+    def tev(self):
+        return self._erdekes['tev']
+
     @datum.setter
-    def datum(self, date):
-        if date >= '2022.12.12':
-            raise ValueError("Ez már jövendőbeli dátum")
-        self._erdekes['datum'] = date
+    def datum(self, value):
+        if type(value) is not datetime.datetime:
+            self._erdekes['datum'] = date.fromisoformat(value)
+        else:
+            datumszo = value.strftime("%Y-%m-%d")
+            self._erdekes['datum'] = datumszo
+
+    @napok.setter
+    def napok(self, napok):
+        self._erdekes['napok'] = napok
+
+    @tkzkr.setter
+    def tkzkr(self, tkzkr):
+        self._erdekes['tkzkr'] = tkzkr
+
+    @zkr.setter
+    def zkr(self, zkr):
+        self._erdekes['zkr'] = zkr
+
+    @ffk.setter
+    def ffk(self, ffk):
+        self._erdekes['ffk'] = ffk
+
+    @kuls.setter
+    def kuls(self, kuls):
+        self._erdekes['kuls'] = kuls
+
+    @kont.setter
+    def kont(self, kont):
+        if kont is not None:
+            szkont = " ".join(kont.split())
+            self._erdekes['kont'] = szkont
+        else:
+            self._erdekes['kont'] = '-'
+
+    @stat.setter
+    def stat(self, stat):
+        self._erdekes['stat'] = stat
+
+    @kulsz.setter
+    def kulsz(self, kulsz):
+        self._erdekes['kulsz'] = kulsz
+
+    @megjegy.setter
+    def megjegy(self, megjegy):
+        if megjegy is not None:
+            szmeg = " ".join(megjegy.split())
+            self._erdekes['megjegy'] = szmeg
+        else:
+            self._erdekes['megjegy'] = '-'
+
+    @tev.setter
+    def tev(self, tev):
+        engtev = ('beépítés', 'bejárás', 'egyeztetés', 'elmarad', 'előadás', 'építés',
+                  'esküvő', 'esőnap', 'felvétel', 'forgatás', 'fotózás', 'főpróba',
+                  'gumicsere', 'hakken', 'karácsony', 'lemondva', 'molyirtás', 'munka',
+                  'pihenő', 'prezentáció', 'próba', 'sajtótájékoztató', 'stream',
+                  'szabadság', 'tanítás', 'társulati', 'temetés', 'turné', 'TV felvétel',
+                  'utazás', 'utazónap', 'világítás')
+        if tev.lower() in engtev:
+            self._erdekes['tev'] = tev
+        else:
+            self._erdekes['tev'] = '-'
+            raise ValueError('Nem ismert tevékenység:', tev)
 
 
 if __name__ == "__main__":
-    bem = Bemeno()
-    bem.datum = "2022.09.01"
-    print(bem.__dict__)
+    bem = Bemeno("2022.09.01", 'hétfő', 'Mindenki', 'Hegedős', 'férfikar',
+                 'Rudi Pötsch', 'joskapista@nagyonfontos.tr', 'Valamilyen állapot',
+                 'Egér haknizik, a többiek dolgoznak', 'Ide írok\n sok \t\t\n szép      megjegyzést', 'előadás')
+    pprint.pprint(bem.__dict__)
+    bem.tev = 'próba'
+    print(bem.tev)
