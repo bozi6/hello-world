346f6f25e9cbf3fd71b14e74404fcdc0a4712e11
Konta Boáz <kontab6@gmail.com>
2023-11-30 21:57:26 +0100

    updated pygdtf demo

--- python_gdtf/lamsza.py
+++ python_gdtf/lamsza.py
@@ -1,13 +1,28 @@
+"""
+GrandMA3 downloaded gdtf file viewer
+"""
+__version__ = "1.6"
+__author__ = "Konta Boáz"
+__author_email__ = "kontab@gmail.com"
+__creation_date__ = '2023.11.30'
+
 import inspect
 import pygdtf
 import os
 from termcolor import cprint
 from colorama import init
-from colorama import Fore
-from colorama import Style
+
 init(autoreset=True)
 
+
 def filescan():
+    """
+    Átnézi a telepített MA3 könyvtárát
+    a letöltött GDTF fileokhoz,
+    ha nem talál ilyet akkor kilép jól.
+    :return: fileok nevű tömb a gdtf fileokkal
+    és root a kezdőkönyvtárral
+    """
     mappa = os.path.expanduser('~')
     if os.name == 'nt':  # Ha Windózban vagyunk
         mappa = "C:/ProgramData/MALightingTechnology/gma3_library/fixturetypes/"
@@ -18,29 +33,42 @@ def filescan():
         for root, dirs, files in os.walk(mappa):
             for file in files:
                 if file.endswith('.gdtf'):
-                    szinesben(root, '/'+file)
+                    # szinesben(root, '/'+file)
                     # gdtfinfo(pygdtf.FixtureType(os.path.join(root, file)))
                     fileok.append(file)
+        if not fileok:
+            print("""Nem találtam gdtf kiterjesztésű fájlokat a: \n{} mappában.
+Lehet nincs telepítve az MA3 applikáció, vagy nincs letöltve
+GDTF lámpainformáció a netről?""".format(mappa))
+            exit(0)
+
     except AttributeError:
         print('Nem találtam fájlokat ...')
-    return fileok,root
+    return fileok, root
+
 
 def print_menu():
+    """
+    Menü kiiratása
+    """
     for key in menu.keys():
         print(key, '--', menu[key])
 
 
-def szinesben(mi, rizsa):
+def szinesben(mi, *args):
     """
     :param mi: A kiírás definíciója pl. név:
-    :param rizsa: A szöveg pl: PAR64
     :return: false
     """
     print(mi, end='')
-    cprint(rizsa, 'green')
+    for arg in args:
+        cprint(arg, 'green')
 
 
 def getmembers(osztaly):
+    """
+    :param osztaly: a kilistázni kívánt osztály neve
+    """
     for i in inspect.getmembers(osztaly):
         if not i[0].startswith('__'):
             if not inspect.ismethod(i[1]):
@@ -48,20 +76,26 @@ def getmembers(osztaly):
 
 
 def gdtfinfo(egy):
+    """
+    :param egy: egy pygdtf osztály
+    """
     # egy = pygdtf.FixtureType('./CPMini.gdtf')
     szinesben('Név: ', egy.name)
     szinesben('Rövid név: ', egy.short_name)
-    szinesben('Hosszú név: ', egy.long_name)
-    szinesben('Leírás: ', egy.description)
+    if egy.description != '':
+        szinesben('Hosszú név: ', egy.long_name)
+    if egy.description != '':
+        szinesben('Leírás: ', egy.description)
     szinesben('Gyártó: ', egy.manufacturer)
-    print('Tárcsák:')
-    tar = egy.wheels
-    i = 0
-    for slot in tar:
-        for egyslot in slot.wheel_slots:
-            print('\t{} - {}  Szín CIE: Y: {}, x {}, y {}'.
-                  format(i, egyslot.name, egyslot.color.Y, egyslot.color.x, egyslot.color.y))
-            i += 1
+    if len(egy.wheels) != 0:
+        print('Tárcsák:')
+        tar = egy.wheels
+        i = 0
+        for slot in tar:
+            for egyslot in slot.wheel_slots:
+                print('\t{}; {} - Szín CIE: Y: {}, x {}, y {}'.
+                      format(i, egyslot.name, egyslot.color.Y, egyslot.color.x, egyslot.color.y, ))
+                i += 1
     szinesben('Színtér: ', egy.color_space.mode)
     print('DMX módok:')
     dmxinfo = pygdtf.utils.get_dmx_modes_info(egy)
@@ -75,7 +109,11 @@ def gdtfinfo(egy):
         #  print(DmxChanInfo)
         for egycsat in DmxChanInfo:
             for param in egycsat:
-                print('\t\tDMX cím: {} - Azononsító: {}'.format(param.get('dmx'), param.get('id')))
+                if param.get('id')[0] == '+':
+                    tizenhatbit = param.get('id').replace('+', 'Fine ')
+                    print('\t\tDMX cím: {} - Id: {}'.format(param.get('dmx'), tizenhatbit))
+                else:
+                    print('\t\tDMX cím: {} - Id: {}'.format(param.get('dmx'), param.get('id')))
     """
     for mod in egy.dmx_modes:
         szinesben('\tMód: ', '{}, attribútumok szám: {}'.format(mod.name, len(mod.dmx_channels)))
@@ -89,7 +127,7 @@ def gdtfinfo(egy):
     szinesben('Fénysugár szög: ', '{}°'.format(feny.beam_angle))
     szinesben('Fogyasztás: ', '{}W'.format(feny.power_consumption))
     szinesben('Színhőmérséklet: ', '{}K'.format(feny.color_temperature))
-    szinesben('Fényerő: ','{}LUX'.format(feny.luminous_flux))
+    szinesben('Fényerő: ', '{}LUX'.format(feny.luminous_flux))
     """print('Változások: ')
     for rev in egy.revisions:
         szinesben('\tDátum: ', rev.date)
@@ -98,29 +136,25 @@ def gdtfinfo(egy):
         print('\t', '-' * 60)
     """
 
+
 if __name__ == '__main__':
-    fileok,root = filescan()
+    fileok, root = filescan()
     menu = {}
     i = 0
     for egyfile in fileok:
         menu[i] = egyfile
         i += 1
     menu[len(menu)] = 'Kilépés.'
-    while(True):
+    while True:
         print_menu()
         option = ''
         try:
             option = int(input('Melyiket nézzük: '))
-        except IndexError:
-            print('Nem találok ilyen számot ...')
-        if option == len(menu)-1:
+        except Exception as err:
+            print('Valami nem lett jó ...', err)
+        if option == len(menu) - 1:
             exit(0)
         if option < len(menu):
-            gdtfinfo(pygdtf.FixtureType(root+'/'+fileok[option]))
+            gdtfinfo(pygdtf.FixtureType(root + '/' + fileok[option]))
         else:
             cprint('Nem találok ilyen számú elemet ...', 'red')
-    """cuccz = (dir(pygdtf))
-    i = 0
-    for egyitem in cuccz:
-        print('{}; {}'.format(i,egyitem))
-        i += 1"""
--- weboldalolvasas/main.py
+++ weboldalolvasas/main.py
@@ -1,34 +1,48 @@
 #!/usr/bin/python3
 import requests
+import os
+import sys
 from bs4 import BeautifulSoup
+from requests.exceptions import HTTPError
 
-url = 'https://www.malighting.com/downloads/products/grandma3/'
-res = requests.get(url)
-html_page = res.content
-print(res)
-soup = BeautifulSoup(html_page, 'html5lib')
+url = "https://www.malighting.com/downloads/products/grandma3/"
 
-# soup = BeautifulSoup(html_page, 'html.parser')
-text = soup.find_all(text=True)
-output = []
-blacklist = [
-    '[document]',
-    'header',
-    'html',
-    'meta',
-    'head',
-    'input'
-    'script'
 
-]
+def oldallekeres(oldalcim):
+    try:
+        res = requests.get(url)
+        res.raise_for_status()
+        return res.content
+    except HTTPError as http_err:
+        print(f"HTTP hiba történt: {http_err}")
+    except Exception as err:
+        print(f"Más hiba történt {err}")
 
-datum = "2022-11-09"
-for t in text:
-    if t.parent.name not in blacklist:
-        output.append(t)
-if output[115][-10:] == datum:
-    print("faszom régi\n Azért itt van amit találtam: ")
-    print(output[115], output[106])
-else:
-    print("Valami újabb dolog történt\nEz van most: ")
-    print(output[115], output[107])
+
+def getverzion() -> object:
+    if sys.platform == "darwin":
+        stream = os.popen('mdls -raw -name kMDItemVersion /Applications/grandMA3.app')
+        verzio: str = stream.read()
+    return verzio
+
+
+if __name__ == "__main__":
+    html_page = oldallekeres(url)
+    soup = BeautifulSoup(html_page, 'html5lib')
+    text = soup.find_all("tr", class_="downloads-table__sub-header js-download-search-sub-header")
+
+    output = ''
+
+    verzio = getverzion()
+
+    for t in text:
+        output += t.text
+
+    print("Meglévő verzió az applikációból: {}\n".format(verzio), "-" * 80)
+    if output.find(verzio) != -1:
+        print("Már megvan.")
+        print("-" * 80)
+        print("Azért itt van amit találtam: \n{}".format(output))
+    else:
+        print("Újabb verzió elérhető\nEz van most: ")
+        print(output)
