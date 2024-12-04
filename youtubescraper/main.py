4cba72db9c664ad5fa1dc5a77e1f289efbba32e8
konta.boaz <kontab6@gmail.com>
2024-12-04 16:08:06 +0100

    new youtube thing

--- youtubescraper/main.py
+++ youtubescraper/main.py
@@ -2,7 +2,7 @@
 #      This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
 #      This is free software, and you are welcome to redistribute it
 #      under certain conditions; type `show c' for details.
-#   Last Modified: 2024. 12. 04. 16:01
+#   Last Modified: 2024. 12. 04. 16:02
 
 import scrapetube
 
